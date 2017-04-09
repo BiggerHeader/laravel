b3d585263c7fb5efffe25218744a4106c203a71e
BiggerHeader <1343652445@qq.com>
2017-04-09 13:52:47 +0800

    Version2.4

--- app/Http/Controllers/Admin/NavsController.php
+++ app/Http/Controllers/Admin/NavsController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Model\Navs;
 use Illuminate\Http\Request;
 
-use App\Http\Requests;
 use App\Http\Controllers\Controller;
 use App\Http\Model\nav;
 use Illuminate\Support\Facades\Input;
